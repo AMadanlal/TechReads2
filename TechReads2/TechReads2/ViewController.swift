aa86d61adfa59b28ba3edd1f596b21e5ae5bb308
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-08-24 11:29:53 +0200

    initial TechReads files

---
+++ TechReads2/TechReads2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechReads2
+//
+//  Created by Akshar Madanlal on 2020/08/24.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
