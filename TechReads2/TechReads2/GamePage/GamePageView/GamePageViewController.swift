7dcead47c6f634f3b7479007663a4ba8382f91b8
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-08-26 11:00:38 +0200

    created initial view files with proper folders

---
+++ TechReads2/TechReads2/GamePage/GamePageView/GamePageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GamePageViewController.swift
+//  TechReads2
+//
+//  Created by Akshar Madanlal on 2020/08/26.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GamePageViewController: UIViewController {
+  
+}
---
+++ TechReads2/TechReads2/SearchPage/SearchPageView/SearchPageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchPageViewController.swift
+//  TechReads2
+//
+//  Created by Akshar Madanlal on 2020/08/26.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchPageViewController: UIViewController {
+  
+}
